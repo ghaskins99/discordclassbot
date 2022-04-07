9dc51966ca7525a2a3e635765dabf5694efb7f88
ghaskins99 <43185853+ghaskins99@users.noreply.github.com>
2022-04-07 01:26:55 -0400

    2022 update

--- bot.js
+++ bot.js
@@ -1,21 +1,24 @@
-const Discord = require(`discord.js`);
+const { Client, Intents } = require(`discord.js`);
 const { token } = require(`./botconfig.json`);
 
 module.exports = class ClassBot {
-	#client = new Discord.Client();
+  #client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-	async initBot() {
-		this.#client.once(`ready`, () => {
-			console.log(`Ready!`);
-		});
-	
-		await this.#client.login(token);
-	}
+  async initBot() {
+    this.#client.once(`ready`, () => {
+      console.log(`Ready!`);
+    });
 
-	async sendSuccessMessage(last, current, courseName)
-	{
-		const channel = await this.#client.channels.fetch(`733183840813842473`);
-		await channel.send(`<@${channel.guild.ownerID}> Class \`${courseName}\` was \`${last ? last : `N/A`}\`, now is \`${current}\``);
-	}
+    await this.#client.login(token);
+  }
 
-}
+  async sendSuccessMessage(last, current, courseName) {
+    const channel = await this.#client.channels.fetch(`733183840813842473`);
+    // channel.guild.ownerID is dead now so
+    await channel.send(
+      `<@206476533135704064> Class \`${courseName}\` was \`${
+        last ? last : `N/A`
+      }\`, now is \`${current}\``
+    );
+  }
+};
--- index.js
+++ index.js
@@ -5,91 +5,102 @@ const puppeteer = require(`puppeteer`);
 const coursesFile = `./courses.json`;
 const courses = require(coursesFile);
 
-const timeoutMins = 5;
+const timeoutMins = 10;
 
 // eslint-disable-next-line
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function check(browser, term, crn) {
-	const page = await browser.newPage();
-	await page.goto(`https://central.carleton.ca/prod/bwysched.p_select_term?wsea_code=EXT`);
-	await page.select(`#term_code`, term);
+  const page = await browser.newPage();
+  await page.goto(
+    `https://central.carleton.ca/prod/bwysched.p_select_term?wsea_code=EXT`
+  );
+  await page.select(`#term_code`, term);
 
-	await Promise.all([
-		page.waitForNavigation(),
-		await page.click(`input[type="submit"]`),
-	]);
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.click(`input[type="submit"]`),
+  ]);
 
-	await page.$eval(`#crn_id`, (el, value) => el.value = value, crn);
+  await page.$eval(`#crn_id`, (el, value) => (el.value = value), crn);
 
-	await Promise.all([
-		page.waitForNavigation(),
-		await page.click(`input[value="Search"]`),
-	]);
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.click(`input[value="Search"]`),
+  ]);
 
-	const msg = await page.$eval(`table tr:nth-child(4) td:nth-child(2)`, el => el.textContent);
-	const courseCode = (await page.$eval(`table tr:nth-child(4) td:nth-child(4)`, el => el.textContent)).trim();
+  const msg = await page.$eval(
+    `table tr:nth-child(4) td:nth-child(2)`,
+    (el) => el.textContent
+  );
+  const courseCode = (
+    await page.$eval(
+      `table tr:nth-child(4) td:nth-child(4)`,
+      (el) => el.textContent
+    )
+  ).trim();
 
-	await page.close();
-	return [msg, courseCode];
+  await page.close();
+  return [msg, courseCode];
 }
 
 async function processCourses(bot, browser) {
-	Object.keys(courses).forEach(term => {
-		courses[term].forEach(async course => {
-			const [currentStatus, courseName] = await check(browser, term, course.code);
+  Object.keys(courses).forEach((term) => {
+    courses[term].forEach(async (course) => {
+      const [currentStatus, courseName] = await check(
+        browser,
+        term,
+        course.code
+      );
 
-			console.log(`${new Date().toISOString()}: ${currentStatus}`);
-
-			if (currentStatus != course.status) {
-				console.log(`Sending new status ${courseName}: ${currentStatus}`);
-				await bot.sendSuccessMessage(course.status, currentStatus, courseName);
-				course.status = currentStatus;
-				fsp.writeFile(coursesFile, JSON.stringify(courses));
-			}
-		});
-	});
+      console.log(`${new Date().toISOString()}: ${currentStatus}`);
+      if (currentStatus != course.status) {
+        console.log(`Sending new status ${courseName}: ${currentStatus}`);
+        await bot.sendSuccessMessage(course.status, currentStatus, courseName);
+        course.status = currentStatus;
+        fsp.writeFile(coursesFile, JSON.stringify(courses));
+      }
+    });
+  });
 }
 
 (async () => {
-	process.once(`SIGTERM`, () => {
-		console.log(`SIGTERM recieved`);
-		process.exit(0);
-	});
+  process.once(`SIGTERM`, () => {
+    console.log(`SIGTERM recieved`);
+    process.exit(0);
+  });
 
-	process.once(`SIGINT`, () => {
-		console.log(`SIGINT recieved`);
-		process.exit(0);
-	});
+  process.once(`SIGINT`, () => {
+    console.log(`SIGINT recieved`);
+    process.exit(0);
+  });
 
-	const resetFlag = process.argv.slice(2)[0];
-	if (resetFlag === `--reset`) {
-		Object.values(courses).forEach(term => {
-			term.forEach(course => {
-				course.status = ``;
-			});
-		});
-		fsp.writeFile(coursesFile, JSON.stringify(courses));
-	}
+  const resetFlag = process.argv.slice(2)[0];
+  if (resetFlag === `--reset`) {
+    Object.values(courses).forEach((term) => {
+      term.forEach((course) => {
+        course.status = ``;
+      });
+    });
+    fsp.writeFile(coursesFile, JSON.stringify(courses));
+  }
 
-	const bot = new ClassBot();
-	await bot.initBot();
-	const browser = await puppeteer.launch({ args: [ `--no-sandbox` ] });
-	// const browser = await puppeteer.launch({
-	// 	executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-	// 	headless: false,
-	// 	args: [
-	// 		'--auto-open-devtools-for-tabs',
-	// 	]
-	// });
-	console.log(`startup ok`);
+  const bot = new ClassBot();
+  await bot.initBot();
+  // const browser = await puppeteer.launch({ args: [ `--no-sandbox` ] });
+  const browser = await puppeteer.launch({
+    executablePath:
+      "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+    headless: false,
+    args: ["--auto-open-devtools-for-tabs"],
+  });
+  console.log(`startup ok`);
 
-	await processCourses(bot, browser);
+  await processCourses(bot, browser);
 
-	setInterval(async () => {
-		await processCourses(bot, browser);
-	}, timeoutMins * 60000);
-
-})().catch(e => console.error(e));
+  setInterval(async () => {
+    await processCourses(bot, browser);
+  }, timeoutMins * 60000);
+})().catch((e) => console.error(e));
